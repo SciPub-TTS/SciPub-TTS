53752364a6058907e82b3517b53156e68de633ec
Huy Phong <touman876@gmail.com>
2026-06-20 16:39:55 +0700

    fix: add Provider into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import { router } from "@/app/router";
 import "@/app/i18n";
 import "@/styles/index.css";
+import {store} from "@/store";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </Provider>
+    </React.StrictMode>,
 );
